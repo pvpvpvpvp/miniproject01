6a9f415f7555ee8c27d7079f8acf8114bdabefc3
pvpvpvpvp <wmrqj@naver.com>
2021-08-04 08:58:29 +0900

    주섣달기 및 불필요 코드 삭제

--- src/mini/pack/MiniEx.java
+++ src/mini/pack/MiniEx.java
@@ -1,5 +1,4 @@
 
-
 package mini.pack;
 
 import java.io.BufferedReader;
@@ -14,39 +13,37 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-
 public class MiniEx {
-	static final String rootPath = System.getProperty("user.dir") + "\\\\"; //이렇게하면 파일 입출력할때 project+파일이름 막을 수 있음
-	static String source = rootPath + "data.txt"; //입력받는 파일
-	static String target = rootPath + "data.txt"; //출력하는 파일 둘이 같게함으로 입력값 그대로 다시 실행시 불러올수있음
+	static final String rootPath = System.getProperty("user.dir") + "\\\\"; // 이렇게하면 파일 입출력할때 project+파일이름 막을 수 있음
+	static String source = rootPath + "data.txt"; // 입력받는 파일
+	static String target = rootPath + "data.txt"; // 출력하는 파일 둘이 같게함으로 입력값 그대로 다시 실행시 불러올수있음
 	// 메소드에서 활용을 위한 정적선언
 	// ArrayList의 특징인 중간인덱스를 삭제하면 당겨오는 특성 때문에 저장 배열로 선언해줌.
-	static List<MiniData> list = new ArrayList<MiniData>(99); 						
+	static List<MiniData> list = new ArrayList<MiniData>(99);
 	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Reader fr = null;
 
 		// 보조 스트림
 		BufferedReader br = null;
-
 		fr = new FileReader(source);
-
 		br = new BufferedReader(fr);
-		
+
 		StringTokenizer st = null;
 		String[] add1 = new String[3];
 		String line = "";
-		while ((line = br.readLine()) != null) {//읽을 라인이 없을때까지 
+		while ((line = br.readLine()) != null) {// 읽을 라인이 없을때까지
 			st = new StringTokenizer(line, ","); // StringTokenizer의 기능인 구별자로 데이터를 나누어 받음
-			for (int i = 0; i < 3; i++) {//받은 데이터를 순차적으로 넣어줌 한Line에 3개의 데이터 값을 가지니 3반복
-				add1[i] = st.nextToken();//넥스트 토큰으로 하면 내부값을 내보낸다.(내부값 지워짐)
+			for (int i = 0; i < 3; i++) {// 받은 데이터를 순차적으로 넣어줌 한Line에 3개의 데이터 값을 가지니 3반복
+				add1[i] = st.nextToken();// 넥스트 토큰으로 하면 내부값을 내보낸다.(내부값 지워짐)
 			}
-			//토큰을 받아 생성된 새배열에서 형식에 맞게 데이터 삽입.
-				MiniData a = new MiniData(add1[0], add1[0 + 1], add1[0 + 2]);
-				list.add(a);//저장된 a(MiniData)형식을 넣어줌
+			// 토큰을 받아 생성된 새배열에서 형식에 맞게 데이터 삽입.
+			MiniData a = new MiniData(add1[0], add1[0 + 1], add1[0 + 2]);
+			list.add(a);// 저장된 a(MiniData)형식을 넣어줌
 		}
-		pro: while (true) {//  pro:는 지정해서 break문을 위해 적어줌
+		pro: while (true) {// pro:는 지정해서 break문을 위해 적어줌
 
 			System.out.println("=========================");
 			System.out.println("=    전화번호 관리 프로그램   =");
@@ -54,7 +51,7 @@ public class MiniEx {
 			System.out.println("1,리스트 2,등록 3,삭제 4,검색 5,종료");
 			int add = sc.nextInt();
 
-			switch (add) { //메소드로 간결하게
+			switch (add) { // 메소드로 간결하게
 			case 1:
 				listOut();
 				break;
@@ -75,10 +72,10 @@ public class MiniEx {
 		}
 	}
 
-	private static void WriteData() throws IOException {//입력 형식에 맞춰서 출력
+	private static void WriteData() throws IOException {// 입력 형식에 맞춰서 출력
 		Writer fw = null;
 		fw = new FileWriter(target);
-		try (BufferedWriter bw = new BufferedWriter(fw)) { 
+		try (BufferedWriter bw = new BufferedWriter(fw)) {
 			for (int i = 0; i < list.size(); i++) {
 				bw.write(list.get(i).getName());
 				bw.write(",");
@@ -94,7 +91,7 @@ public class MiniEx {
 		System.out.println("=====================");
 	}
 
-	public static void InputData() { //데이터를 입력받아서 MinnData형식에 넣고 리스트에 추가
+	public static void InputData() { // 데이터를 입력받아서 MinnData형식에 넣고 리스트에 추가
 		System.out.printf("<2,등록>%n이름:");
 		String name = sc.next();
 		System.out.printf("휴대전화:");
@@ -106,9 +103,10 @@ public class MiniEx {
 
 	}
 
-	public static void listOut() {	//MineData에 listout(int listIndex) 메소드를 이용해서 출력
-		for (int i = 0; i < list.size(); i++) { 
-			list.get(i).listout(i);; //lsit.get(index)로 값을 출력		
+	public static void listOut() { // MineData에 listout(int listIndex) 메소드를 이용해서 출력
+		for (int i = 0; i < list.size(); i++) {
+			list.get(i).listout(i);
+			; // lsit.get(index)로 값을 출력
 		}
 	}
 
@@ -116,16 +114,21 @@ public class MiniEx {
 		int Delnumber = 0;
 		System.out.print("지울번호 입력하세요");
 		Delnumber = sc.nextInt();
-		list.remove(Delnumber - 1);// 값보정 
+		list.remove(Delnumber - 1);// 값보정
 	}
 
-	public static void Find() { 
+	public static void Find() {
 		System.out.print("검색 단어 입력");
 		String name = sc.next();
-		for (int i1 = 0; i1 < list.size(); i1++) { //리스트 내의 모든값을 찾기위해 반복
-			if (list.get(i1).getName().contains(name)) {//입력한 값 name이 getname과 같으면
-				list.get(i1).listout(i1); //출력
+		int j = 0;
+		for (int i1 = 0; i1 < list.size(); i1++) { // 리스트 내의 모든값을 찾기위해 반복
+			if (list.get(i1).getName().contains(name)) {// 입력한 값 name이 getname과 같으면
+				list.get(i1).listout(i1); // 출력
+				j++;
 			}
+		}
+		if (j == 0) {
+			System.out.println("결과값이 없습니다");
 		}
 	}
 }
